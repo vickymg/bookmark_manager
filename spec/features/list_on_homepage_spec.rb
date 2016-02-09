4cf63676aecb882cee6ffd3a867ba425c3f3ce93
Hannah Smyth-Osbourne <h.smythosbourne@gmail.com>
2016-02-09 10:29:35 +0000

    First feature test ammended

--- spec/features/list_on_homepage_spec.rb
+++ spec/features/list_on_homepage_spec.rb
@@ -2,8 +2,12 @@ require 'spec_helper'
 
   feature 'list of URLS on homepage' do
 
-    scenario 'going onto homepage' do
-      visit '/'
+    scenario 'can see existing links on homepage' do
+      Link.create(url: 'http:www.makersacademy.com',title: 'Makers Academy')
+      visit '/links'
       expect(page).to have_content 'Your favourites:'
+      within 'ul#links' do
+        expect(page).to have_content('Makers Academy')
+      end
     end
   end
