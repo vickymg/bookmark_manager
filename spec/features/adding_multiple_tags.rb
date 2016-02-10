24338f6c295abb9469b34d36cab3731d2e804855
Jeremy Barrass <jeremybarrass@Jeremys-MacBook-Pro.local>
2016-02-10 18:01:26 +0000

    Adding multiple tags to links test writing in progress.

--- spec/features/adding_multiple_tags.rb
+++ spec/features/adding_multiple_tags.rb
@@ -5,9 +5,10 @@ feature 'tagging a link' do
     visit '/link/add-new'
     fill_in 'bookmark_name', with: 'Favourite title'
     fill_in 'url', with: 'www.weeee!.co.uk'
-    fill_in 'tag', with: 'Orcs!'
+    fill_in 'tag', with: 'Orcs!; 
     click_button 'create link'
     link = Link.first
-    expect(link.tag.map(&:tag)).to include('Orcs!')
+    within 'ul#link' do
+      expect(page).to have_content('bookmark name')
   end
 end
