726ecbae0315b84ad543f5769985d7801cc3364d
Hannah Smyth-Osbourne <h.smythosbourne@gmail.com>
2016-02-10 11:01:36 +0000

    Changes

--- bookmark_manager/app/models/link.rb
+++ bookmark_manager/app/models/link.rb
@@ -1,5 +1,9 @@
+
+
 require 'data_mapper'
 require 'dm-postgres-adapter'
+
+
 
 class Link
   include DataMapper::Resource
@@ -10,6 +14,6 @@ class Link
 
 end
 
-DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
+DataMapper.setup(:default,ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
 DataMapper.finalize
 DataMapper.auto_upgrade!
