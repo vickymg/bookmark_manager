2f04b859181e3adad7d163b4e6d0b6f3a2802acd
Rufus Raghunath <rufusraghunath@gmail.com>
2016-02-10 12:07:25 +0000

    added 'http://' autocompletion feature

--- bookmark_app.rb
+++ bookmark_app.rb
@@ -14,25 +14,26 @@ class BookmarkApp < Sinatra::Base
 
     @tags = params[:Tags].downcase
 
+    if params[:URL].include?('http://') || params[:URL].include?('https://')
+      @url = params[:URL]
+    else
+      @url = 'http://' + params[:URL]
+    end
+
     #create new tags if neccessary
     @tags.split(', ').each do |tag|
       if Tag.all(name: tag).empty?
-        Tag.create(name: tag, urls: params[:URL])
+        Tag.create(name: tag, urls: @url)
       else
         @link = Tag.first(name: tag).urls
-        Tag.first(name: tag).update(urls: (@link + ', ' + params[:URL]))
+        Tag.first(name: tag).update(urls: (@link + ', ' + @url))
       end
     end
 
     #create new links if neccessary
-    if Link.all(url: params[:URL]).empty?
-      Link.create(title: params[:Title], url: params[:URL], tags: @tags)
+    if Link.all(url: @url).empty?
+      Link.create(title: params[:Title], url: @url, tags: @tags)
     end
-
-
-
-
-
 
     redirect to '/'
   end
